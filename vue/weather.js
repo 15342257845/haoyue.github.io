f14636ed5dd84e6c3de0bd8746f0980f6963091c
Ektu <90824445+15342257845@users.noreply.github.com>
2022-04-20 17:10:33 +0800

    Update weather.js

--- vue/weather.js
+++ vue/weather.js
@@ -44,51 +44,51 @@ var app = new Vue({
                 .catch(function (err) { })
         },
         queryWeather() {
-            this.forecastList = [];
+          //  this.forecastList = [];
 
-            axios.get(`http://wthrcdn.etouch.cn/weather_mini?city=${this.city}`)
-                .then(res => {
-                    console.log(res);
-                    this.forecastList = res.data.data.forecast;
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-                .finally(() => { });
+          //  axios.get(`http://wthrcdn.etouch.cn/weather_mini?city=${this.city}`)
+             //   .then(res => {
+            //        console.log(res);
+                    this.forecastList =  [
+             {
+                 type: '多云',
+                 low: '低温16℃',
+                 high: '高温25℃',
+                 date: '1日星期一'
+             },
+             {
+                 type: '晴',
+                 low: '低温20℃',
+                 high: '高温30℃',
+                 date: '2日星期二'
+             },
+             {
+                 type: '小雨',
+                 low: '低温15℃',
+                 high: '高温20℃',
+                 date: '3日星期三'
+             },
+             {
+                type: '阴',
+                 low: '低温13℃',
+                 high: '高温20℃',
+                 date: '4日星期四'
+             },
+             {
+                 type: '多云',
+                 low: '低温16℃',
+                 high: '高温25℃',
+                 date: '5日星期五'
+             },
+         ];;
+             //   })
+             //   .catch(err => {
+            //       console.log(err);
+            //    })
+            //    .finally(() => { });
         },
 
-        // [
-        //     {
-        //         type: '多云',
-        //         low: '低温16℃',
-        //         high: '高温25℃',
-        //         date: '1日星期一'
-        //     },
-        //     {
-        //         type: '晴',
-        //         low: '低温20℃',
-        //         high: '高温30℃',
-        //         date: '2日星期二'
-        //     },
-        //     {
-        //         type: '小雨',
-        //         low: '低温15℃',
-        //         high: '高温20℃',
-        //         date: '3日星期三'
-        //     },
-        //     {
-        //         type: '阴',
-        //         low: '低温13℃',
-        //         high: '高温20℃',
-        //         date: '4日星期四'
-        //     },
-        //     {
-        //         type: '多云',
-        //         low: '低温16℃',
-        //         high: '高温25℃',
-        //         date: '5日星期五'
-        //     },
-        // ];
+       
         clickSearch(city) {
             this.city = city;
             this.queryWeather();
